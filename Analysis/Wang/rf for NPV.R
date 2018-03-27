852a3fa2a65b9f6624b0fee71766ebf41dfc1d1a
tbillman <trb9259@uncw.edu>
2018-03-27 12:53:50 -0400

    RF/RGLM!!

--- Analysis/Wang/rf for NPV.R
+++ Analysis/Wang/rf for NPV.R
@@ -2,16 +2,13 @@
 library(randomForest); #install.packages('randomForest');
 library(ranger);
 library(Rborist);#install.packages('Rborist')
-fn<-"/Users/yishiwang/Documents/Research Since 2013/Research 2018/Project with Thomas Billman/OrgNPVs.csv"
+library(randomGLM)
+fn<-"/Users/Thomas/Documents/Github/Wang499/OrgNPVs2.csv"
 raw<-read.csv(fn, header=TRUE);
-rfdat<-subset.rfdata.frame(raw, select=-c(FirstPmt,Maturity.rfdate,MSA.Code, Loan.Sequence.Number,Seller.Name,Servicer.Name,Super.Conforming))
+rfdat<-subset.data.frame(raw, select=-c(FirstPmt,Maturity.Date,MSA.Code, Loan.Sequence.Number,Seller.Name,Servicer.Name,Super.Conforming))
 rfdat<-rfdat[complete.cases(rfdat),]
- rfdat<-rfdat[1:1000,];
+rfdat<-rfdat[1:1000,];
 n<-dim(rfdat)[1]; m=dim(rfdat)[2];
-# dat$NPV<-as.factor(dat$NPV>0);
-rfdat$NPV<-cut(rfdat$NPV, c(-Inf, 0,10^4,3*10^4,Inf),lables=c(1:4));
-table(rfdat$NPV)
-
 
 set.seed(12345)
 
@@ -19,27 +16,36 @@ cv.size<-5;rfdat.gp<-sample(1:cv.size,n,T) # table(dat.gp)
 
 cv.rf.ranger<-function(x)
 {
+  print(x)
   fidx<-which(rfdat.gp==x);fold.size<-length(fidx)
   train.rfdat<-rfdat[-fidx,];
   test.rfdat<-rfdat[fidx,];
-  f.rf.mod<-ranger(NPV~., rfdata=train.rfdat,num.trees=500);
-  f.pred<-predict(f.rf.mod,test.rfdat)$predictions;
-  accu<-sum(test.rfdat$NPV==f.pred)/fold.size;
-  return(c(accu,fold.size))
+  f.rf.mod<-ranger(NPV~., data=train.rfdat,num.trees=500);
+  r2 <- f.rf.mod$r.squared
+  return(c(r2,fold.size))
   
 }
-
-cv.rf.Rbo<-function(x){
-  fidx<-which(rfdat.gp==x);fold.size<-length(fidx)
-  train.rfdat<-rfdat[-fidx,];
-  test.rfdat<-rfdat[fidx,];
-  pt <- PreFormat(train.rfdat[,-m])
-  f.rf.mod<-Rborist(pt, train.rfdat[,m] ,nTree=100);
-  f.pred<-predict(f.rf.mod,test.rfdat[,-m])$predictions;
-  accu<-sum(test.rfdat$NPV==f.pred)/fold.size;
-  return(c(accu,fold.size))
-}
-
 # added the sort so the order of your df is maintained
-res.rf<-lapply(c(1:cv.size), FUN=cv.rf.Rbo)
+res.rf<-lapply(c(1:cv.size), FUN=cv.rf.ranger)
 res.rf
+
+fact <- sapply(1:(m),function(x){
+  is.factor(rfdat[,x])
+})
+rglm.dat <- rfdat[,which(fact == 0)]
+m <- dim(rglm.dat)[2]
+
+cv.rglm<-function(x)
+{
+  print(x)
+  fidx<-which(rfdat.gp==x);fold.size<-length(fidx)
+  train.rgdat<-rglm.dat[-fidx,];
+  test.rgdat<-rglm.dat[fidx,];
+  rglm.mod<-randomGLM(x = train.rgdat[,-m], y = train.rgdat[,m], 
+                      xtest = test.rgdat[,-m], nCandidateCovariates = m-1);
+  r2 <- summary(lm(test.rgdat[,m] ~ rglm.mod$predictedTest))$r.squared
+  return(c(r2,fold.size))
+  
+}
+res.rglm <- lapply(c(1:cv.size), FUN=cv.rglm)
+res.rglm
